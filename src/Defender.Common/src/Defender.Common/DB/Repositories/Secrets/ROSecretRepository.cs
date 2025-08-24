be8bc259e99fdfeb6b74a320de144c45baafff07
Danil <47692381+SelDanilEv@users.noreply.github.com>
2025-08-24 13:06:51 +0200

    Add MongoSecret entity and read-only secret repository
    Introduced MongoSecret entity and ROSecretRepository for managing secrets in MongoDB. Updated .gitignore for secrets directory and simplified Dockerfile build steps by removing debug echo commands.

---
+++ src/Defender.Common/src/Defender.Common/DB/Repositories/Secrets/ROSecretRepository.cs
@@ -0,0 +1,37 @@
+﻿using Defender.Common.Configuration.Options;
+using Defender.Common.Consts;
+using Defender.Common.DB.Model;
+using Defender.Common.Entities.Secrets;
+using Defender.Common.Helpers;
+using Defender.Common.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace Defender.Common.DB.Repositories.Secrets;
+
+internal class ROSecretRepository(
+    IOptions<MongoDbOptions> mongoOption)
+    : BaseMongoRepository<MongoSecret>(
+        new MongoDbOptions(
+            ConstValues.SecretManagementServiceMongoDBName,
+            mongoOption?.Value!)),
+    IMongoSecretAccessor
+{
+    public async Task<string> GetSecretValueByNameAsync(string secretName)
+    {
+        var secret = await GetSecretByNameAsync(secretName);
+
+        if (secret == null)
+        {
+            return string.Empty;
+        }
+
+        return await CryptographyHelper.DecryptStringAsync(secret?.Value!, secretName);
+    }
+
+    public async Task<MongoSecret> GetSecretByNameAsync(string secretName)
+    {
+        var findModelRequest = FindModelRequest<MongoSecret>.Init(x => x.SecretName, secretName);
+
+        return await GetItemAsync(findModelRequest);
+    }
+}
---
+++ src/Defender.Common/src/Defender.Common/Entities/Secrets/MongoSecret.cs
@@ -0,0 +1,24 @@
+﻿using Defender.Common.Enums;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Defender.Common.Entities.Secrets;
+
+public record MongoSecret : IBaseModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; }
+    public string? SecretName { get; set; }
+    public string? Value { get; set; }
+
+    public static MongoSecret FromSecret(Secret secret, string value)
+    {
+        return new MongoSecret { SecretName = secret.ToString(), Value = value };
+    }
+
+    public static MongoSecret FromSecretName(string secretName, string value)
+    {
+        return new MongoSecret { SecretName = secretName, Value = value };
+    }
+}
